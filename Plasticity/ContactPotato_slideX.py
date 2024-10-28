11da2b949025d95ca8e451434374fb984ca2bc5d
diego.hurtado1 <diego.hurtado@uni.lu>
2024-10-28 13:58:56 +0100

    adapted potato scrip for argsparsing

--- Plasticity/ContactPotato_slideX.py
+++ Plasticity/ContactPotato_slideX.py
@@ -8,7 +8,27 @@ import matplotlib.pyplot as plt
 from pdb import set_trace
 from math import pi
 
+
 from time import time
+import argparse
+
+
+# Argument parsing
+parser = argparse.ArgumentParser(description='Process a data for the 2d contact model.')
+parser.add_argument('--min_method', type=str, required=True, help='minimization method: BFGS, LBFGSNN')
+parser.add_argument('--mesh', type=int, required=True, help='choose mesh 5, 10 or 15')
+parser.add_argument('--plastic', type=int, required=True, help='boolean for plastic')
+
+args = parser.parse_args()
+
+# Calculate subspace bounds
+minimization_method = args.min_method
+mesh = args.mesh
+plastic = args.plastic
+
+# minimization_method = "BFGS"
+# mesh = 15
+# plastic = 1
 
 
 ####################
@@ -17,13 +37,16 @@ from time import time
 os.chdir(sys.path[0])
 
 # BLOCK
-# mesh_blk   = meshio.read("../Meshes/Cubes/cube5x5x5.msh")
-mesh_blk   = meshio.read("../Meshes/Cubes/cube10x10x10.msh")
-# mesh_blk   = meshio.read("../Meshes/Cubes/cube15x15x15.msh")
+mesh_blk   = meshio.read("../Meshes/Cubes/cube"+str(mesh)+"x"+str(mesh)+"x"+str(mesh)+".msh")
 
 X_blk     = mesh_blk.points
 hexas_blk = mesh_blk.cells_dict['hexahedron']
-blk = FEAssembly(X_blk,hexas_blk, name= "BLOCK",recOuters=False)
+
+if plastic:
+    blk = FEAssembly(X_blk,hexas_blk, name= "BLOCK",recOuters=False,plastic_param=[0.01,0.05,1.0])
+else:
+    blk = FEAssembly(X_blk,hexas_blk, name= "BLOCK",recOuters=False)
+
 blk.Youngsmodulus = 0.05
 blk.Translate([0.0,0.0,3.5])
 
@@ -64,10 +87,12 @@ BCs = [cond_bd1, cond_bd2]
 slave   = [blk , blk_bottom]
 master = [ptt,ptt_highernodes]
 
-contact1 = Contact(slave, master, kn=5, C1Edges = False, maxGN = 0.001,f0=0.1)       # (slave, master) inputs can be surfaces as well
+# contact1 = Contact(slave, master, kn=5, C1Edges = False, maxGN = 0.001,f0=0.1)       # (slave, master) inputs can be surfaces as well
+contact1 = Contact(slave, master, kn=1e2, C1Edges = False, maxGN = 0.001,f0=0.1)       # (slave, master) inputs can be surfaces as well
 
 ### MODEL ###
-model = FEModel([blk, ptt], [contact1], BCs)           # [bodies, contacts, BCs, opts*]
+subname = "_"+("plastic" if plastic else "elastic")+"_"+minimization_method+"_"+str(mesh)
+model = FEModel([blk, ptt], [contact1], BCs,subname =subname)           # [bodies, contacts, BCs, opts*]
 
 ndofs = 3*(len(X_blk)+len(ptt.X))
 ptt.surf.ComputeGrgPatches(np.zeros(ndofs),range(len(ptt.surf.nodes)))
@@ -85,7 +110,7 @@ ptt.surf.ComputeGrgPatches(np.zeros(ndofs),range(len(ptt.surf.nodes)))
 
 t0 = time()
 
-model.Solve(TimeSteps=100, recover=True, ForcedShift=False,max_iter=20)
+model.Solve(TimeSteps=100, recover=False, ForcedShift=False,max_iter=20,minimethod=minimization_method)
 
 print("this took",time()-t0,"seconds to compute")
 
--- Plasticity/pseudo2d.py
+++ Plasticity/pseudo2d.py
@@ -13,10 +13,6 @@ from time import time
 import argparse
 
 
-
-
-
-
 # Argument parsing
 parser = argparse.ArgumentParser(description='Process a data for the 2d contact model.')
 parser.add_argument('--min_method', type=str, required=True, help='minimization method: BFGS, LBFGSNN')
@@ -30,17 +26,10 @@ minimization_method = args.min_method
 mesh = args.mesh
 plastic = args.plastic
 
-
-
-
-
-
-
-
-
 # minimization_method = "BFGS"
 # mesh = 15
 # plastic = 1
+
 
 ####################
 ### GETTING DATA ###
@@ -52,13 +41,10 @@ mesh_blk   = meshio.read("../Meshes/Block_pseudo2d_"+str(mesh)+".msh")
 X_blk     = mesh_blk.points
 hexas_blk = mesh_blk.cells_dict['hexahedron']
 
-
 if plastic:
     blk = FEAssembly(X_blk,hexas_blk, name= "BLOCK",recOuters=False,plastic_param=[0.01,0.05,1.0])
 else:
     blk = FEAssembly(X_blk,hexas_blk, name= "BLOCK",recOuters=False)
-
-
 
 blk.Youngsmodulus = 0.05
 blk.Translate([-3,0.0,2.6])
@@ -79,16 +65,6 @@ base_top = base.SelectFlatSide("+z")
 base.X[21,2] += 0.5
 base.X[39,2] += 0.5
 
-# base.X[21,2] += 0.25
-# base.X[39,2] += 0.25
-# base.X[22,2] -= 0.25
-# base.X[40,2] -= 0.25
-
-
-
-# base.X[21,2] += 0.45
-# base.X[39,2] += 0.45
-
 ndofs = 3*(len(X_blk)+len(base.X))
 
 
@@ -102,11 +78,6 @@ blk_top     = blk.SelectFlatSide("+z")
 lead_face = blk.SelectFlatSide("-y")
 front_face = blk.SelectFlatSide("+y")
 
-
-# blk.X[lead_face,1] = -0.05
-# blk.X[front_face,1] = 0.05
-
-
 slave_nodes = list(set(blk_bottom).intersection(set(lead_face)))
 blk_top     = list(set(blk_top   ).intersection(set(lead_face)))
 
@@ -118,7 +89,7 @@ for ii,node in enumerate(lead_face):
 
 
 # Construction of Transformation matrices for pseudo-2d out of the 3d implementation
-# Ns : Matrix used to enforce symmetry between faces at -Y and +Y faces
+# Ns : Matrix used to enforce symmetry between nodes at -Y and +Y faces
 # Nt : Matrix used to extract X and Z of the leader face (at -Y) in the 3d model as the pseudo-2d model
 rs = np.zeros(2*(len(blk.X)))
 cs = np.zeros(2*(len(blk.X)))
@@ -142,8 +113,6 @@ N = [Ns,Nt]
 
 ### BOUNDARY CONDITIONS ###  [body, nodes, type, directions, values, times(*)]
 cond_bd1 = [base, base.SelectAll(), "dirichlet", "xyz", [0.0, 0.0, 0.0]              ]      # Base: static and rigid
-# cond_bd2 = [blk , blk_top         , "dirichlet",  "xz", [0.0, 0.0,-0.25], [0.0, 0.2] ]      # Block: Indentation
-# cond_bd3 = [blk , blk_top         , "dirichlet",  "xz", [6.0, 0.0, 0.0], [0.2, 1.0]  ]      # Block: Displacement
 cond_bd2 = [blk , blk_top         , "dirichlet",  "xz", [0.0, 0.0,-0.25], [0.0, 0.1] ]      # Block: Indentation
 cond_bd3 = [blk , blk_top         , "dirichlet",  "xz", [6.0, 0.0, 0.0 ], [0.1, 0.9] ]      # Block: Displacement
 cond_bd4 = [blk , blk_top         , "dirichlet",  "xz", [0.0, 0.0, 0.25], [0.9, 1.0 ] ]      # Block: Indentation
@@ -184,7 +153,6 @@ base.surf.ComputeGrgPatches(np.zeros(ndofs),base_top,exactNodesGiven=True)
 
 t0 = time()
 
-# model.Solve(TimeSteps=100, recover=True,max_iter=15, IterUpdate = False)
 model.Solve(TimeSteps=100, recover=False,max_iter=15, IterUpdate = False,minimethod=minimization_method)
 
 print("this took",time()-t0,"seconds to compute")
