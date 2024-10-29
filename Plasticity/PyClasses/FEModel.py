b02dda9e3a2a2a630b518f7ce0d4de78aa7bd18d
diego.hurtado1 <diego.hurtado@uni.lu>
2024-10-29 09:48:21 +0100

    bug settrace bfgs

--- Plasticity/PyClasses/FEModel.py
+++ Plasticity/PyClasses/FEModel.py
@@ -583,9 +583,6 @@ class FEModel:
         while np.linalg.norm(f_2 - f_new) > 0 and np.linalg.norm(f_2) > tol:
             self.COUNTS[4] += 1
 
-            if iter>260:
-                set_trace()
-
             iter += 1
             print("ITER:",iter)
             f_old = f_new.copy()
